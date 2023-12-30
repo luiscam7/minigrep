d1a47744d75ef24e0d9761678c911604a8dfa070
luiscam7 <lcamargo2007@gmail.com>
2023-12-30 15:16:00 -0800

    error messaging

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::{env, process};
 use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -13,10 +13,7 @@ fn main() {
     println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
-
-
-
